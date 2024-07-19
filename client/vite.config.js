c667cdc46ce1a271e814b7849a2a3ad5bd9ec6d0
sannu kumar <125146922+sannu1565@users.noreply.github.com>
2024-07-19 15:10:30 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -5,6 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 // https://vitejs.dev/config/
 export default defineConfig({
   server:{
+     host: '0.0.0.0',
   proxy:{
     '/api' : 'http://localhost:5173',
   },
