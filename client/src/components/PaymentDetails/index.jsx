1d36fe7624eb2c50c2a1a3147db59db0006fb173
sannu kumar <125146922+sannu1565@users.noreply.github.com>
2024-07-19 15:38:20 +0530

    Update index.jsx

--- client/src/components/PaymentDetails/index.jsx
+++ client/src/components/PaymentDetails/index.jsx
@@ -7,7 +7,7 @@ const PaymentDetails = () => {
   useEffect(() => {
     const fetchBookingData= async () => {
         try {
-            const response = await axios.get('http://localhost:3001/booking');
+            const response = await axios.get('https://geettourism.onrender.com/booking');
             setBookingData(response.data);
         } catch (error) {
             console.error('Error fetching data:', error);
